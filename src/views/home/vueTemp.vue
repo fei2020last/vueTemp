0f78bacef0667a8d23415fbbd3493f2922dd3ef7
fei2020last <1753686308@qq.com>
2021-10-18 13:20:50 +0800

    vue3.x页面结构模板

---
+++ src/views/home/vueTemp.vue
@@ -0,0 +1,54 @@
+<!-- vue3.x的模板示例 -->
+<!-- vue3.x中生命周期变化：
+           `beforeDestroy` 变成了 `beforeUnmount`
+           `destroyed` 变成了 `unmounted`
+-->
+<template>
+  <div class="vueTemp"></div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  name: 'vueTemp',
+  props: {},
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {}
+  },
+  //计算属性
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建之前
+  beforeCreate() {},
+  //生命周期 - 在绑定元素的 attribute 或事件监听器被应用之前调用（可以访问当前this实例）
+  created() {},
+  // 在绑定元素的父组件挂载之前调用
+  beforeMount() {},
+  // 绑定元素的父组件被挂载时调用
+  mounted() {},
+  // 在包含组件的 VNode 更新之前调用
+  beforeUpdate() {},
+  // 在包含组件的 VNode 及其子组件的 VNode 更新之后调用
+  updated() {},
+  // 在绑定元素的父组件卸载之前调用
+  beforeUnmount() {},
+  // 卸载绑定元素的父组件时调用
+  unmounted() {},
+  //如果页面有keep-alive缓存功能，这个函数会触发
+  activated() {}
+}
+</script>
+<style lang="less">
+//@import url(); 引入公共css类
+
+.vueTemp {
+}
+</style>
